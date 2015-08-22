akien-mga/dynadungeons

commit 66146dc8ef09089209c05564fc63ef6fe0cd1037

Make collectibles destroyed by bomb explosions
Fixes #4.
This is currently by using a dirty workaround to upstream issue 1906.
Since Area2D does not seem to report collisions, we place a dummy collider
under the collectible in a separate tilemap, set not to collide with players.

scripts/global.gd
@@ -94,8 +94,8 @@ func load_config():
 			event.type = InputEvent.KEY
 			event.scancode = scancode
 			# TODO: Handle multiple events per action in a better way
-			InputMap.erase_action(action)
-			InputMap.add_action(action)
+			for old_event in InputMap.get_action_list(action):
+				InputMap.action_erase_event(action, old_event)
 			InputMap.action_add_event(action, event)
 
 func set_from_cfg(config, section, key, fallback):

scenes/level.xscn
@@ -1,7 +1,8 @@
 <?xml version="1.0" encoding="UTF-8" ?>
-<resource_file type="PackedScene" subresource_count="6" version="1.1" version_name="Godot Engine v1.1.rc3.custom_build">
+<resource_file type="PackedScene" subresource_count="7" version="1.1" version_name="Godot Engine v1.1.stable.custom_build">
 	<ext_resource path="res://scripts/level.gd" type="Script"></ext_resource>
 	<ext_resource path="res://sprites/tileset.xml" type="TileSet"></ext_resource>
+	<ext_resource path="res://sprites/dummy-collider.xml" type="TileSet"></ext_resource>
 	<ext_resource path="res://music/dynadingo.ogg" type="AudioStream"></ext_resource>
 	<ext_resource path="res://sprites/box.png" type="Texture"></ext_resource>
 	<ext_resource path="res://fonts/firasans-book-32.fnt" type="Font"></ext_resource>
@@ -12,7 +13,7 @@
 			<string> "conns" </string>
 			<int_array  len="0"> 				 </int_array>
 			<string> "names" </string>
-			<string_array  len="86">
+			<string_array  len="87">
 				<string> "Level" </string>
 				<string> "Node2D" </string>
 				<string> "_import_path" </string>
@@ -46,6 +47,7 @@
 				<string> "collision/mask" </string>
 				<string> "tile_data" </string>
 				<string> "Destructible" </string>
+				<string> "DummyCollider" </string>
 				<string> "PlayerManager" </string>
 				<string> "BombManager" </string>
 				<string> "CollectibleManager" </string>
@@ -101,11 +103,11 @@
 				<string> "percent_visible" </string>
 			</string_array>
 			<string> "node_count" </string>
-			<int> 11 </int>
+			<int> 12 </int>
 			<string> "nodes" </string>
-			<int_array  len="353"> 				-1, -1, 1, 0, -1, 12, 2, 0, 3, 1, 4, 2, 5, 2, 6, 3, 7, 4, 8, 5, 9, 6, 10, 7, 11, 1, 12, 8, 13, 9, 0, 0, 0, 15, 14, -1, 1, 2, 0, 0, 1, 0, 17, 16, -1, 25, 2, 0, 3, 1, 4, 2, 5, 2, 6, 3, 7, 4, 8, 5, 9, 6, 10, 7, 11, 1, 18, 7, 19, 10, 20, 11, 21, 12, 22, 13, 23, 14, 24, 7, 25, 15, 26, 15, 27, 2, 28, 5, 29, 3, 30, 3, 31, 16, 13, 17, 0, 1, 0, 17, 32, -1, 25, 2, 0, 3, 1, 4, 2, 5, 2, 6, 3, 7, 4, 8, 5, 9, 6, 10, 7, 11, 1, 18, 7, 19, 10, 20, 11, 21, 12, 22, 13, 23, 14, 24, 7, 25, 15, 26, 15, 27, 2, 28, 5, 29, 3, 30, 3, 31, 18, 13, 19, 0, 0, 0, 15, 33, -1, 1, 2, 0, 0, 0, 0, 15, 34, -1, 1, 2, 0, 0, 0, 0, 15, 35, -1, 1, 2, 0, 0, 0, 0, 37, 36, -1, 7, 2, 0, 38, 20, 39, 15, 40, 1, 41, 5, 42, 15, 43, 15, 0, 0, 0, 1, 44, -1, 11, 2, 0, 3, 15, 4, 2, 5, 2, 6, 3, 7, 4, 8, 5, 9, 6, 10, 3, 11, 1, 13, 21, 0, 8, 0, 45, 45, -1, 21, 2, 0, 3, 1, 4, 2, 5, 2, 6, 3, 7, 22, 8, 5, 9, 6, 10, 7, 11, 1, 46, 23, 47, 1, 48, 4, 49, 15, 50, 15, 51, 3, 52, 3, 53, 7, 54, 24, 55, 15, 56, 25, 0, 8, 0, 57, 57, -1, 33, 2, 0, 3, 1, 4, 2, 5, 2, 6, 3, 58, 26, 59, 26, 60, 27, 61, 28, 62, 0, 63, 0, 64, 0, 65, 0, 66, 1, 67, 1, 68, 14, 69, 2, 70, 29, 71, 30, 72, 31, 73, 5, 74, 32, 75, 2, 76, 5, 77, 5, 78, 15, 79, 15, 80, 33, 81, 3, 82, 7, 83, 1, 84, 15, 85, 2, 0 </int_array>
+			<int_array  len="408"> 				-1, -1, 1, 0, -1, 12, 2, 0, 3, 1, 4, 2, 5, 2, 6, 3, 7, 4, 8, 5, 9, 6, 10, 7, 11, 1, 12, 8, 13, 9, 0, 0, 0, 15, 14, -1, 1, 2, 0, 0, 1, 0, 17, 16, -1, 25, 2, 0, 3, 1, 4, 2, 5, 2, 6, 3, 7, 4, 8, 5, 9, 6, 10, 7, 11, 1, 18, 7, 19, 10, 20, 11, 21, 12, 22, 13, 23, 14, 24, 7, 25, 15, 26, 15, 27, 2, 28, 5, 29, 3, 30, 3, 31, 16, 13, 17, 0, 1, 0, 17, 32, -1, 25, 2, 0, 3, 1, 4, 2, 5, 2, 6, 3, 7, 4, 8, 5, 9, 6, 10, 7, 11, 1, 18, 7, 19, 10, 20, 11, 21, 12, 22, 13, 23, 14, 24, 7, 25, 15, 26, 15, 27, 2, 28, 5, 29, 3, 30, 3, 31, 18, 13, 19, 0, 1, 0, 17, 33, -1, 24, 2, 0, 3, 15, 4, 2, 5, 2, 6, 3, 7, 4, 8, 5, 9, 6, 10, 7, 11, 1, 18, 7, 19, 20, 20, 11, 21, 12, 22, 13, 23, 14, 24, 7, 25, 15, 26, 15, 27, 2, 28, 5, 29, 14, 30, 3, 31, 21, 0, 0, 0, 15, 34, -1, 1, 2, 0, 0, 0, 0, 15, 35, -1, 1, 2, 0, 0, 0, 0, 15, 36, -1, 1, 2, 0, 0, 0, 0, 38, 37, -1, 7, 2, 0, 39, 22, 40, 15, 41, 1, 42, 5, 43, 15, 44, 15, 0, 0, 0, 1, 45, -1, 11, 2, 0, 3, 15, 4, 2, 5, 2, 6, 3, 7, 4, 8, 5, 9, 6, 10, 3, 11, 1, 13, 23, 0, 9, 0, 46, 46, -1, 21, 2, 0, 3, 1, 4, 2, 5, 2, 6, 3, 7, 24, 8, 5, 9, 6, 10, 7, 11, 1, 47, 25, 48, 1, 49, 4, 50, 15, 51, 15, 52, 3, 53, 3, 54, 7, 55, 26, 56, 15, 57, 27, 0, 9, 0, 58, 58, -1, 33, 2, 0, 3, 1, 4, 2, 5, 2, 6, 3, 59, 28, 60, 28, 61, 29, 62, 30, 63, 0, 64, 0, 65, 0, 66, 0, 67, 1, 68, 1, 69, 14, 70, 2, 71, 31, 72, 32, 73, 33, 74, 5, 75, 34, 76, 2, 77, 5, 78, 5, 79, 15, 80, 15, 81, 35, 82, 3, 83, 7, 84, 1, 85, 15, 86, 2, 0 </int_array>
 			<string> "variants" </string>
-			<array  len="34" shared="false">
+			<array  len="36" shared="false">
 				<node_path> "" </node_path>
 				<bool> True </bool>
 				<real> 1 </real>
@@ -123,7 +125,7 @@
 						<string> "2D" </string>
 						<dictionary  shared="false">
 							<string> "ofs" </string>
-							<vector2> -312.321, -108.58 </vector2>
+							<vector2> -361.171, -112.388 </vector2>
 							<string> "snap_grid" </string>
 							<bool> True </bool>
 							<string> "snap_offset" </string>
@@ -277,6 +279,8 @@
 					<string> "_edit_lock_" </string>
 					<bool> True </bool>
 				</dictionary>
+				<resource  resource_type="TileSet" path="res://sprites/dummy-collider.xml">  </resource>
+				<int_array  len="0"> 					 </int_array>
 				<resource  resource_type="AudioStream" path="res://music/dynadingo.ogg">  </resource>
 				<dictionary  shared="false">
 					<string> "_edit_group_" </string>
@@ -294,7 +298,7 @@
 				<resource  resource_type="Font" path="res://fonts/firasans-book-32.fnt">  </resource>
 				<color> 0.461845, 0, 0.158786, 1 </color>
 				<color> 0, 0, 0, 1 </color>
-				<real> 6 </real>
+				<real> 5 </real>
 				<string> "Player 1 wins !" </string>
 			</array>
 			<string> "version" </string>

scripts/bomb.gd
@@ -53,8 +53,8 @@ func find_chain_and_collisions(trigger_bomb, exceptions = []):
 		if (raycast.empty()):
 			# No collision in range, so full range for the animation
 			self.anim_ranges[key] = self.bomb_range
-		elif (raycast.collider == level.tilemap_destr or raycast.collider == level.tilemap_indestr):
-			# Destructible or indestructible in range, they limit the animation and should be handled differently
+		elif (raycast.collider.get_parent() == level.map_manager):
+			# Destructible, indestructible or cellectible (dummy collider) in range, they limit the animation
 			var target_cell_pos = level.tilemap_destr.world_to_map(raycast.position + dir[key]*global.TILE_SIZE*0.5)
 			var distance_rel = target_cell_pos - self.cell_pos
 			self.anim_ranges[key] = dir[key].x*distance_rel.x + dir[key].y*distance_rel.y - 1
@@ -62,13 +62,20 @@ func find_chain_and_collisions(trigger_bomb, exceptions = []):
 				trigger_bomb.destruct_cells.append(target_cell_pos)
 			elif (raycast.collider == level.tilemap_indestr and not target_cell_pos in trigger_bomb.indestruct_cells):
 				trigger_bomb.indestruct_cells.append(target_cell_pos)
+			else:
+				# Remove the dummy collider
+				level.tilemap_dummy.set_cell(target_cell_pos.x, target_cell_pos.y, -1)
+				# Remove the corresponding collectible
+				for collectible in level.collectible_manager.get_children():
+					if (level.world_to_map(collectible.get_pos()) == target_cell_pos):
+						collectible.queue_free()
 		else:
 			print("Warning: Unexpected collision with '", raycast.collider, "' for the bomb explosion.")
 	
 	for bomb in new_bombs:
 		bomb.find_chain_and_collisions(trigger_bomb, exceptions)
 
-### Explosion animation
+### Explosion animation and logic
 
 func start_animation():
 	for bomb in [self] + self.chained_bombs:
@@ -135,6 +142,8 @@ func stop_animation():
 				collectible.effect = global.collectibles.types[index]
 				collectible.set_pos(level.map_to_world(pos))
 				level.collectible_manager.add_child(collectible)
+				# Add a dummy collider under the collectible
+				level.tilemap_dummy.set_cell(pos.x, pos.y, 0)
 			level.tilemap_destr.set_cell(pos.x, pos.y, -1)
 
 ### Process

scripts/collectible.gd
@@ -2,12 +2,16 @@ extends Area2D
 
 # Nodes
 var global
+var level
 
 # Member variables
 var effect = "bomb_increase"
 
 func _on_body_enter(body):
 	if (body extends global.player_script):
+		# Remove dummy collider
+		level.tilemap_dummy.set_cell(level.world_to_map(get_pos()).x, level.world_to_map(get_pos()).y, -1)
+		# Apply effect
 		if (effect == "bomb_increase" and body.bomb_quota < global.MAX_BOMBS):
 			body.bomb_quota += 1
 		elif (effect == "flame_increase" and body.bomb_range < global.MAX_FLAMERANGE):
@@ -23,4 +27,5 @@ func _on_AnimationPlayer_finished():
 
 func _ready():
 	global = get_node("/root/global")
+	level = get_node("/root").get_node("Level")
 	get_node("Sprite").set_texture(load("res://sprites/pickups/" + effect + ".png"))

scripts/level.gd
@@ -8,6 +8,7 @@ var bomb_manager
 var collectible_manager
 var tilemap_destr
 var tilemap_indestr
+var tilemap_dummy
 
 ### Member variables
 var exploding_bombs = []
@@ -38,6 +39,7 @@ func _ready():
 	collectible_manager = self.get_node("CollectibleManager")
 	tilemap_destr = map_manager.get_node("Destructible")
 	tilemap_indestr = map_manager.get_node("Indestructible")
+	tilemap_dummy = map_manager.get_node("DummyCollider")
 	
 	# Instance players
 	var player

sprites/dummy-collider.xml
@@ -1,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8" ?>
+<resource_file type="TileSet" subresource_count="3" version="1.1" version_name="Godot Engine v1.1.stable.custom_build">
+	<ext_resource path="res://sprites/icons/arrow-up.png" type="Texture"></ext_resource>
+	<resource type="RectangleShape2D" path="local://1">
+		<real name="custom_solver_bias"> 0 </real>
+		<vector2 name="extents"> 16, 16 </vector2>
+
+	</resource>
+	<main_resource>
+		<string name="0/name"> "InvisibleCollider" </string>
+		<resource name="0/texture" resource_type="Texture" path="res://sprites/icons/arrow-up.png">  </resource>
+		<vector2 name="0/tex_offset"> 0, 0 </vector2>
+		<resource name="0/material"></resource>		<rect2 name="0/region"> 0, 0, 0, 0 </rect2>
+		<vector2 name="0/occluder_offset"> 16, 16 </vector2>
+		<resource name="0/occluder"></resource>		<vector2 name="0/navigation_offset"> 16, 16 </vector2>
+		<resource name="0/navigation"></resource>		<vector2 name="0/shape_offset"> 16, 16 </vector2>
+		<array name="0/shapes" len="1" shared="false">
+			<resource  resource_type="Shape2D" path="local://1">  </resource>
+		</array>
+
+	</main_resource>
+</resource_file>

commit b0abdc920491cec44b3fdb822dfa29346eaed06c

Fix display of custom key bindings in controls mapping UI
It seems this feature was broken since 66146dc. I am not sure to understand why it broke,
but we can revert back to the old system for the time being.

scripts/global.gd
@@ -110,10 +110,11 @@ func load_config():
 			event = InputEvent()
 			event.type = InputEvent.KEY
 			event.scancode = scancode
-			# Replace old actions by the new one
+			# Replace old actions by the new one - apparently erasing the old action
+			# works better to get the control buttons properly initialised in the UI
 			# TODO: Handle multiple events per action in a better way
-			for old_event in InputMap.get_action_list(action):
-				InputMap.action_erase_event(action, old_event)
+			InputMap.erase_action(action)
+			InputMap.add_action(action)
 			InputMap.action_add_event(action, event)
 
 func set_from_cfg(config, section, key):