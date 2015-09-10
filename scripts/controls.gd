697dac67cd9cf63649867fd47015581ea140de46
Rémi Verschelde <rverschelde@gmail.com>
2015-09-10 23:04:47 +0200

    Add GPLv3+ license header to all scripts
    Fixes #27

--- scripts/controls.gd
+++ scripts/controls.gd
@@ -1,3 +1,14 @@
+# This file is part of the DynaDungeons game
+# Copyright (C) 2015  Rémi Verschelde and contributors
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 extends Control
 
 ### Nodes
